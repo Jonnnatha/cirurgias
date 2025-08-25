276d5f58f0b9352d3bd6d1cc6c17c4b28c788eaf
Jonnatha <42788538+Jonnnatha@users.noreply.github.com>
2025-08-25 16:20:07 -0300

    refactor: replace email identifier in password reset tokens

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->foreignId('user_id')->primary()->constrained()->cascadeOnDelete();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
