baa4eedc75f7beb04dbccd6e64f96f09e5db942d
Jonnatha <42788538+Jonnnatha@users.noreply.github.com>
2025-08-25 14:27:19 -0300

    Adjust auth tests for custom user fields

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -23,7 +23,7 @@ class AuthenticationTest extends TestCase
         $user = User::factory()->create();
 
         $response = $this->post('/login', [
-            'email' => $user->email,
+            'nome' => $user->nome,
             'password' => 'password',
         ]);
 
@@ -36,7 +36,7 @@ class AuthenticationTest extends TestCase
         $user = User::factory()->create();
 
         $this->post('/login', [
-            'email' => $user->email,
+            'nome' => $user->nome,
             'password' => 'wrong-password',
         ]);
 
