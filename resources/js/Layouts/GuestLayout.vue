0ffbbf133ded7f8dec7d75dceee852ba76a1b895
Jonnatha <42788538+Jonnnatha@users.noreply.github.com>
2025-08-26 14:35:19 -0300

    Remove user registration features

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -19,17 +19,6 @@ import { Link } from '@inertiajs/vue3';
                     >
                         Login
                     </Link>
-                    <Link
-                        :href="route('register')"
-                        :class="[
-                            'px-3 py-2 text-sm font-medium',
-                            route().current('register')
-                                ? 'text-indigo-600 border-b-2 border-indigo-600'
-                                : 'text-gray-600 hover:text-gray-800'
-                        ]"
-                    >
-                        Register
-                    </Link>
                 </div>
             </div>
         </nav>
