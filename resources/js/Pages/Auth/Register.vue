6b8a25ece9d5e6ec7a97d8e3f95112d80bfa062b
Jonnatha <42788538+Jonnnatha@users.noreply.github.com>
2025-08-26 11:57:23 -0300

    feat: enhance register form layout

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -23,80 +23,94 @@ const submit = () => {
 <template>
     <GuestLayout>
         <Head title="Registrar" />
+        <div class="max-w-md mx-auto">
+            <form
+                @submit.prevent="submit"
+                class="bg-white p-8 rounded-lg shadow-lg flex flex-col gap-4"
+            >
+                <h1 class="text-2xl font-semibold text-center text-gray-800">
+                    Criar conta
+                </h1>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="nome" value="Nome" />
+                <div class="flex flex-col gap-1">
+                    <InputLabel for="nome" value="Nome" />
 
-                <TextInput
-                    id="nome"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.nome"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
+                    <TextInput
+                        id="nome"
+                        type="text"
+                        class="mt-1 block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
+                        v-model="form.nome"
+                        required
+                        autofocus
+                        autocomplete="name"
+                    />
 
-                <InputError class="mt-2" :message="form.errors.nome" />
-            </div>
+                    <InputError class="text-sm" :message="form.errors.nome" />
+                </div>
 
-            <div class="mt-4">
-                <InputLabel for="hierarquia" value="Hierarquia" />
+                <div class="flex flex-col gap-1">
+                    <InputLabel for="hierarquia" value="Hierarquia" />
 
-                <TextInput
-                    id="hierarquia"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.hierarquia"
-                    required
-                />
+                    <TextInput
+                        id="hierarquia"
+                        type="text"
+                        class="mt-1 block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
+                        v-model="form.hierarquia"
+                        required
+                    />
 
-                <InputError class="mt-2" :message="form.errors.hierarquia" />
-            </div>
+                    <InputError class="text-sm" :message="form.errors.hierarquia" />
+                </div>
 
-            <div class="mt-4">
-                <InputLabel for="senha" value="Senha" />
+                <div class="flex flex-col gap-1">
+                    <InputLabel for="senha" value="Senha" />
 
-                <TextInput
-                    id="senha"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.senha"
-                    required
-                    autocomplete="new-password"
-                />
+                    <TextInput
+                        id="senha"
+                        type="password"
+                        class="mt-1 block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
+                        v-model="form.senha"
+                        required
+                        autocomplete="new-password"
+                    />
 
-                <InputError class="mt-2" :message="form.errors.senha" />
-            </div>
+                    <InputError class="text-sm" :message="form.errors.senha" />
+                </div>
 
-            <div class="mt-4">
-                <InputLabel for="senha_confirmation" value="Confirmar Senha" />
+                <div class="flex flex-col gap-1">
+                    <InputLabel
+                        for="senha_confirmation"
+                        value="Confirmar Senha"
+                    />
 
-                <TextInput
-                    id="senha_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.senha_confirmation"
-                    required
-                    autocomplete="new-password"
-                />
+                    <TextInput
+                        id="senha_confirmation"
+                        type="password"
+                        class="mt-1 block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
+                        v-model="form.senha_confirmation"
+                        required
+                        autocomplete="new-password"
+                    />
 
-                <InputError class="mt-2" :message="form.errors.senha_confirmation" />
-            </div>
+                    <InputError class="text-sm" :message="form.errors.senha_confirmation" />
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    :href="route('login')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Já registrado?
-                </Link>
+                <div class="flex items-center justify-between pt-4">
+                    <Link
+                        :href="route('login')"
+                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Já registrado?
+                    </Link>
 
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Registrar
-                </PrimaryButton>
-            </div>
-        </form>
+                    <PrimaryButton
+                        :class="{ 'opacity-25': form.processing }"
+                        :disabled="form.processing"
+                    >
+                        Registrar
+                    </PrimaryButton>
+                </div>
+            </form>
+        </div>
     </GuestLayout>
 </template>
