735fca1e1e8e943fb12df7e96aea8fcb0b01beef
Jonnatha <42788538+Jonnnatha@users.noreply.github.com>
2025-09-03 16:13:01 -0300

    Allow admins to set doctor in exam reservations

--- app/Http/Controllers/ExamReservationController.php
+++ app/Http/Controllers/ExamReservationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\ExamReservation;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ExamReservationController extends Controller
@@ -20,11 +21,28 @@ class ExamReservationController extends Controller
             'surgery_request_id' => ['nullable', 'exists:surgery_requests,id'],
             'exam_type' => ['required', 'string'],
             'date' => ['required', 'date'],
+            'doctor_id' => ['nullable', 'exists:users,id'],
         ]);
 
+        $doctorId = $request->input('doctor_id', $request->user()->id);
+
+        if ($request->user()->hasRole('admin')) {
+            $request->validate([
+                'doctor_id' => ['required'],
+            ]);
+
+            abort_unless(
+                User::find($doctorId)?->hasRole('medico'),
+                422,
+                'The selected doctor_id is invalid.'
+            );
+        }
+
+        unset($data['doctor_id']);
+
         $reservation = ExamReservation::create([
-            'doctor_id' => $request->user()->id,
             ...$data,
+            'doctor_id' => $doctorId,
         ]);
 
         return response()->json($reservation, 201);
